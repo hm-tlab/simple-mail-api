d3d6469f151f7dc2c5896d4cce8fb8d979f37c43
hm-tlab <carp01hiro@gmail.com>
2022-07-02 12:31:26 +0900

    構築スタック作成

--- bin/simple-mail-api.ts
+++ bin/simple-mail-api.ts
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { SimpleMailApiStack } from '../lib/simple-mail-api-stack';
+import {
+  App, Environment
+} from 'aws-cdk-lib';
+import { ApiGatewayStack } from '../lib/api-gateway';
+import { StepFunctionsStack } from '../lib/step-functions';
 
-const app = new cdk.App();
-new SimpleMailApiStack(app, 'SimpleMailApiStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const app = new App();
+const env: Environment = {
+  account: process.env.CDK_DEFAULT_ACCOUNT!,
+  region: process.env.CDK_DEFAULT_REGION!
+}
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const stepFunctionsStack = new StepFunctionsStack(app, 'StepFunctionsStack', {
+  env,
+  srcAddressSsmRef: '/ses/senderIdentity',
+  definitionSrcPath: './assets/stepFunctions/sendMail.json'
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new ApiGatewayStack(app, 'ApiGatewayStack', {
+  env,
+  stateMachineArn: stepFunctionsStack.stateMachine.attrArn
 });
